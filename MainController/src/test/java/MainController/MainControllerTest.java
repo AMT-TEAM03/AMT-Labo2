8578d42728719e1264fe2046cb4a07584a62f449
Jeremie Melly <melly.jeremie@gmail.com>
2022-12-23 13:36:00 +0100

    Remove springboot dependencies

--- MainController/src/test/java/MainController/MainControllerTest.java
+++ MainController/src/test/java/MainController/MainControllerTest.java
@@ -2,17 +2,9 @@ package MainController;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.springframework.http.HttpStatus;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -20,22 +12,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 //import MainController.utils.ErrorResponse;
 //import MainController.CloudProvider.AWS.JSON.AwsReckognitionResult;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import java.beans.Transient;
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.util.Base64;
 import java.util.List;
 
-@SpringBootTest
-@AutoConfigureMockMvc
 class MainControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
     private ObjectMapper mapper = new ObjectMapper();
 
     static String _base64Img;
@@ -44,55 +27,55 @@ class MainControllerTest {
         "testing1234.png"
     };
 
-    private void cleanup() throws Exception {
-        for (String i : OBJECT_KEY_LIST) {
-            mockMvc.perform(delete("/v1/object/" + i)).andExpect(status().isOk());
-        }
-    }
+    // private void cleanup() throws Exception {
+    //     for (String i : OBJECT_KEY_LIST) {
+    //         mockMvc.perform(delete("/v1/object/" + i)).andExpect(status().isOk());
+    //     }
+    // }
 
-    @BeforeTestClass
-    void beforeAll() throws Exception {
-        // Encode an image in a base64 like string
-        // image path declaration
-        String imgPath = "./src/main/resources/coucou.jpg";
-        // read image from file
-        FileInputStream stream = new FileInputStream(imgPath);
-        // get byte array from image stream
-        int bufLength = 2048;
-        byte[] buffer = new byte[2048];
-        byte[] data;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        int readLength;
-        while ((readLength = stream.read(buffer, 0, bufLength)) != -1) {
-            out.write(buffer, 0, readLength);
-        }
-        data = out.toByteArray();
-        _base64Img = Base64.getEncoder().withoutPadding().encodeToString(data);
-        out.close();
-        stream.close();
-        // cleanup of possible old test DataObject
-        cleanup();
-    }
+    // @BeforeTestClass
+    // void beforeAll() throws Exception {
+    //     // Encode an image in a base64 like string
+    //     // image path declaration
+    //     String imgPath = "./src/main/resources/coucou.jpg";
+    //     // read image from file
+    //     FileInputStream stream = new FileInputStream(imgPath);
+    //     // get byte array from image stream
+    //     int bufLength = 2048;
+    //     byte[] buffer = new byte[2048];
+    //     byte[] data;
+    //     ByteArrayOutputStream out = new ByteArrayOutputStream();
+    //     int readLength;
+    //     while ((readLength = stream.read(buffer, 0, bufLength)) != -1) {
+    //         out.write(buffer, 0, readLength);
+    //     }
+    //     data = out.toByteArray();
+    //     _base64Img = Base64.getEncoder().withoutPadding().encodeToString(data);
+    //     out.close();
+    //     stream.close();
+    //     // cleanup of possible old test DataObject
+    //     cleanup();
+    // }
 
-    @BeforeEach
-    void beforeEach() throws Exception {
-        mockMvc.perform(post("/v1/object")
-            .param("name", OBJECT_KEY_LIST[0])
-            .param("image", _base64Img)
-        ).andExpect(status().isOk());
-    }
+    // @BeforeEach
+    // void beforeEach() throws Exception {
+    //     mockMvc.perform(post("/v1/object")
+    //         .param("name", OBJECT_KEY_LIST[0])
+    //         .param("image", _base64Img)
+    //     ).andExpect(status().isOk());
+    // }
 
-    @AfterEach
-    void afterEach() throws Exception {
-        mockMvc.perform(delete("/v1/object/" + OBJECT_KEY_LIST[0]))
-                .andExpect(status().isOk());
-    }
+    // @AfterEach
+    // void afterEach() throws Exception {
+    //     mockMvc.perform(delete("/v1/object/" + OBJECT_KEY_LIST[0]))
+    //             .andExpect(status().isOk());
+    // }
     
 
     // Nothing exists
     @Test
     public void Nothing_Exist(){
-        
+
     }
 
     // UploadObject_ObjectDoesntExist_Success
