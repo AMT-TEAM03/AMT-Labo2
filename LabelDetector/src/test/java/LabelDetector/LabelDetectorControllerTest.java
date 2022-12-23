d77e2621d2affed766ad016a0d669b1ab664df57
Jeremie Melly <melly.jeremie@gmail.com>
2022-12-23 14:08:23 +0100

    Fix: CI subdirectory for MainController

--- LabelDetector/src/test/java/LabelDetector/LabelDetectorControllerTest.java
+++ LabelDetector/src/test/java/LabelDetector/LabelDetectorControllerTest.java
@@ -5,13 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.fasterxml.jackson.core.Base64Variants;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import LabelDetector.controller.request.LabelRequest;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
